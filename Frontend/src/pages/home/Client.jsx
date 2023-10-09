14c54e6e0ac3a8d5a6af26e99881e7cf56119914
Suraj Maharjan <surajmaharjan235@hotmail.comm>
2023-10-09 12:04:29 +0545

    work done of clinets

--- Frontend/src/pages/home/Client.jsx
+++ Frontend/src/pages/home/Client.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';
 import 'owl.carousel/dist/assets/owl.theme.default.css';
+import axios from 'axios';
 
 const Client = () => {
     const images = [
@@ -31,14 +32,40 @@ const Client = () => {
             }
         }
     };
+
+    const [clien, setClien] = useState([]);
+
+    const ClientsData = async () => {
+        try {
+            const response = await axios.get(
+                "http://127.0.0.1:8000/api/navigations/"
+            );
+            // Filter the response data by status and page_type
+            if (response.data) {
+                const clienData = response.data.filter(
+                    (item) => item.status === "Publish" && item.page_type === "Client"
+                );
+                setClien(clienData); // Assuming you want to slice the filtered data
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    useEffect(() => {
+        // Axios GET request to fetch data
+        ClientsData();
+    }, []);
+    // console.log(clien)
+
     return (
         <section className='py-20'>
             <div className="container flex flex-col items-center">
-                <h2 className="lg:text-4xl sm:text-3xl text-2xl font-semibold mb-8 relative after:absolute after:w-[80%] after:content-[''] after:h-[4px] after:bg-sky-700 after:bottom-[-8px] after:left-[10%] after:rounded-md before:absolute before:w-[4px] before:h-[4px] before:bg-white before:content-[''] before:z-10 before:bottom-[-8px] custom-animation" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">Our Clients</h2>
+                <h2 className="lg:text-4xl sm:text-3xl text-2xl font-semibold mb-8 relative after:absolute after:w-[80%] after:content-[''] after:h-[4px] after:bg-sky-700 after:bottom-[-8px] after:left-[10%] after:rounded-md before:absolute before:w-[4px] before:h-[4px] before:bg-white before:content-[''] before:z-10 before:bottom-[-8px] custom-animation" data-aos="fade-up" data-aos-duration="1000" data-aos-once="true">{clien.length > 0 ? clien[0].caption : ''}</h2>
                 <OwlCarousel className="owl-theme" {...options}>
-                    {images.map((image, index) => (
+                    {clien.map((image, index) => (
                         <div className="item flex justify-center items-center clients-image" key={index}>
-                            <img src={image} alt="clients" />
+                            <img src={image.bannerimage} alt="clients" />
                         </div>
                     ))}
                 </OwlCarousel>
